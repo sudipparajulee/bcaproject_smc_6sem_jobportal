ffd69336dc129df4467499a52659a4be73f412af
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-09 07:38:39 +0545

    Dashboard UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('content')
-    <form action="{{route('login')}}">
+    <form action="{{route('login')}}" method="POST">
         @csrf
         <input type="email" placeholder="Your Email" name="email">
         <input type="password" placeholder="Your Password" name="password">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 @section('title','Dashboard')
 @section('content')
+    <div class="grid grid-cols-3 gap-5">
+        <div class="bg-gradient-to-r from-blue-50 to-red-50 p-4 rounded-lg shadow-md">
+            <h2 class="text-xl font-bold mb-2">Total Users</h2>
+            <div class="flex justify-between items-center">
+                <span class="text-gray-500 font-bold">TU</span>
+                <span class="text-3xl font-semibold">1000</span>
+            </div>
+        </div>
+        <div class="bg-gradient-to-r from-yellow-50 to-blue-50 p-4 rounded-lg shadow-md">
+            <h2 class="text-xl font-bold mb-2">Total Companies</h2>
+            <div class="flex justify-between items-center">
+                <span class="text-gray-500 font-bold">TU</span>
+                <span class="text-3xl font-semibold">1000</span>
+            </div>
+        </div>
+        <div class="bg-gradient-to-r from-red-50 to-green-50 p-4 rounded-lg shadow-md">
+            <h2 class="text-xl font-bold mb-2">Total Jobs</h2>
+            <div class="flex justify-between items-center">
+                <span class="text-gray-500 font-bold">TU</span>
+                <span class="text-3xl font-semibold">1000</span>
+            </div>
+        </div>
 
+    </div>
 @endsection
