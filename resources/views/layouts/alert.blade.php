aab1fd857f28ae02bcd279a737d4bd7251e2d104
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-16 07:41:30 +0545

    updates

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -2,10 +2,19 @@
 <div class="fixed right-4 top-4 px-10 py-2 border border-blue-500 text-blue-600 rounded-lg shadow-lg font-bold text-lg bg-blue-50" id="alert">
     <p>{{session('success')}}</p>
 </div>
+@endif
+{{-- Handle validation errors  --}}
+@if ($errors->any())
+    <div class="fixed right-4 top-4 px-10 py-2 border border-red-500 text-red-600 rounded-lg shadow-lg font-bold text-lg bg-red-50" id="alert">
+            @foreach ($errors->first() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+    </div>
+@endif
 <script>
     // Hide the alert after 3 seconds
     setTimeout(function() {
         document.getElementById('alert').style.display = 'none';
     }, 3000);
 </script>
-@endif
+
--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -16,7 +16,7 @@
             <td class="p-2 border">{{$loop->iteration}}</td>
             <td class="p-2 border">{{$tag->name}}</td>
             <td class="p-2 border">
-                <a href="#" class="bg-blue-600 text-white px-4 py-1 rounded mr-2">Edit</a>
+                <a onclick="showEditModal('{{$tag->id}}','{{$tag->name}}')" class="bg-blue-600 text-white px-4 py-1 rounded mr-2 cursor-pointer">Edit</a>
                 <a class="bg-red-600 text-white px-4 py-1 rounded">Delete</a>
             </td>
         </tr>
@@ -35,6 +35,20 @@
         </form>
     </div>
 
+    {{-- Edit Tag Modal  --}}
+    <div id="editmodal" class="hidden fixed inset-0 bg-blue-500 bg-opacity-25 backdrop-blur-sm items-center justify-center">
+        <form action="" method="POST" class="p-4 bg-white rounded-lg w-1/2">
+            @csrf
+            <h1 class="font-bold text-xl">Edit Tag</h1>
+            <input type="text" id="name" name="name" placeholder="Enter Tag" class="block w-full rounded ">
+            <div class="flex justify-end mt-4">
+                <input onclick="hideEditModal()" type="button" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 mr-2" value="Cancel">
+                <input type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" value="Add Tag">
+            </div>
+        </form>
+    </div>
+    {{-- End of Edit Tag Modal --}}
+
     <script>
         function showModal(){
             document.getElementById('modal').classList.remove('hidden');
@@ -43,6 +57,18 @@
         function hideModal(){
             document.getElementById('modal').classList.remove('flex');
             document.getElementById('modal').classList.add('hidden');
+        }
+    </script>
+
+    <script>
+        function showEditModal(id,name){
+            document.getElementById('editmodal').classList.remove('hidden');
+            document.getElementById('editmodal').classList.add('flex');
+            document.getElementById('name').value = name;
+        }
+        function hideEditModal(){
+            document.getElementById('editmodal').classList.remove('flex');
+            document.getElementById('editmodal').classList.add('hidden');
         }
     </script>
 @endsection
